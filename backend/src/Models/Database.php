4c895c4181ef6b46e9a5255684c0693dc4bca9c0
Thomas Steuck <Thomas.Steuck@smartinsurtech.de>
2022-05-19 15:38:50 +0200

    added index and webserver configuration

---
+++ backend/src/Models/Database.php
@@ -0,0 +1,16 @@
+<?php
+class Database {
+    protected $connection = null;
+
+    public function __construct() {
+        try {
+            $this->connection = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME);
+         
+            if ( mysqli_connect_errno()) {
+                throw new Exception("Could not connect to database.");   
+            }
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());   
+        }   
+    }
+}
