6f51df4ae1a548a669d5a54b2ab1bf5105639c2d
Thomas Steuck <Thomas.Steuck@smartinsurtech.de>
2022-06-28 14:11:44 +0200

    entity definition added

---
+++ backend/src/Models/Entities/member.php
@@ -0,0 +1,3 @@
+// bekommt Array mit Sportarten, die ausgeführt werden
+// bekommt Info (boolean + ma_id) in welcher Mannschaft er spielt
+// bekommt Info (boolean + ma_id) ob er ein Trainer ist und von welcher
