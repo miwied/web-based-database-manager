a1fe03faf0cb3e3de863703e7d33592e41fe41a9
Noodle693 <steuckthomas@gmail.com>
2022-07-12 21:29:01 +0200

    cleaned up login and implemented validation

--- frontend/src/app/Components/login/login.component.ts
+++ frontend/src/app/Components/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
-import { first } from 'rxjs/operators';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { SportsClubApiService } from 'src/app/services/sportsClub-api.service';
-import { MatDialog } from '@angular/material/dialog';
 import { SnackBarService } from 'src/app/services/snack-bar.service';
 
 @Component({
@@ -13,75 +16,79 @@ import { SnackBarService } from 'src/app/services/snack-bar.service';
 })
 export class LoginComponent implements OnInit {
   selection: string = 'Login';
-  angForm: FormGroup;
+  loginForm: FormGroup;
   constructor(
-    private dialog: MatDialog,
     private fb: FormBuilder,
     private dataService: SportsClubApiService,
     private router: Router,
     private snackBarService: SnackBarService
   ) {
-    this.angForm = this.fb.group({
-      username: ['', [Validators.required, Validators.minLength(1)]],
-      password: ['', Validators.required],
+    this.loginForm = this.fb.group({
+      username: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
     });
   }
 
   ngOnInit() {}
 
-  postdata(angForm: FormGroup) {
-    switch (this.selection) {
-      case 'Login':
-        this.dataService
-          .userLogin(angForm.value.username, angForm.value.password)
-          .subscribe({
-            next: (data) => {
-              const redirect = this.dataService.redirectUrl
-                ? this.dataService.redirectUrl
-                : '/table';
-              this.router.navigate([redirect]);
-            },
-            error: (e) => {
-              console.log(e);
-              this.snackBarService.showSnackBar(
-                'Nutzername oder Passwort ungültig',
-                'start',
-                'bottom',
-                'snackbar-error'
-              );
-            },
-          });
-        break;
-      case 'Register':
-        this.dataService
-          .userRegistration(angForm.value.username, angForm.value.password)
-          .subscribe({
-            next: (value) =>
-              this.snackBarService.showSnackBar(
-                'Nutzer angelegt',
-                'start',
-                'bottom',
-                'snackbar-success'
-              ),
-            error: (e) =>
-              e.status == 409
-                ? this.snackBarService.showSnackBar(
-                    'Nutzer ist bereits angelegt',
-                    'start',
-                    'bottom',
-                    'snackbar-error'
-                  )
-                : console.log(e),
-          });
-        break;
-      default:
-        break;
+  postdata(loginForm: FormGroup) {
+    if (loginForm.valid) {
+      switch (this.selection) {
+        case 'Login':
+          this.dataService
+            .userLogin(loginForm.value.username, loginForm.value.password)
+            .subscribe({
+              next: (data) => {
+                const redirect = this.dataService.redirectUrl
+                  ? this.dataService.redirectUrl
+                  : '/table';
+                this.router.navigate([redirect]);
+              },
+              error: (e) => {
+                console.log(e);
+                this.snackBarService.showSnackBar(
+                  'Nutzername oder Passwort ungültig',
+                  'start',
+                  'bottom',
+                  'snackbar-error'
+                );
+              },
+            });
+          break;
+        case 'Register':
+          this.dataService
+            .userRegistration(
+              loginForm.value.username,
+              loginForm.value.password
+            )
+            .subscribe({
+              next: (value) =>
+                this.snackBarService.showSnackBar(
+                  'Nutzer angelegt',
+                  'start',
+                  'bottom',
+                  'snackbar-success'
+                ),
+              error: (e) =>
+                e.status == 409
+                  ? this.snackBarService.showSnackBar(
+                      'Nutzer ist bereits angelegt',
+                      'start',
+                      'bottom',
+                      'snackbar-error'
+                    )
+                  : console.log(e),
+            });
+          break;
+        default:
+          break;
+      }
     }
-  }
-  getUserName() {
-    return this.angForm.get('username');
-  }
-  getPassword() {
-    return this.angForm.get('password');
   }
 }
