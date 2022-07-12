b9d619b3a1f4783c549ed14fdf29f3552463cf1d
Noodle693 <steuckthomas@gmail.com>
2022-07-12 20:59:22 +0200

    removed old commented code from member.ts (models folder)

--- frontend/src/app/models/member.ts
+++ frontend/src/app/models/member.ts
@@ -1,21 +1,5 @@
 export interface IMember {
   memberId: number;
-  // firstName: string;
-  // lastName: string;
-  // zipCode: number;
-  // city: string;
-  // gender: string;
-  // feeId: number;
-  // feeGroup: string;
-  // fee: number;
-  // sportIds: any;
-  // sports: any;
-  // isPlayer: boolean;
-  // playerTeamId: number;
-  // playerTeamName: any;
-  // isTrainer: boolean;
-  // trainerTeamId: number;
-  // trainerTeamName: any;
   [key: string]: any;
 }
 
